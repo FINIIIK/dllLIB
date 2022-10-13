c2a15b6f2673c5822f17280cddc0da66e8c6b6e8
FINIIIK <94627737+FINIIIK@users.noreply.github.com>
2022-10-14 02:13:17 +0500

    Add files via upload

---
+++ dllLIB/dllLIB/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace dllLIB
+{
+    public class Class1
+    {
+        public static void PlusFunc(double x, double y)
+        {
+            double Sum = x + y;
+            Console.WriteLine("{0} прибавить {1} = {2}", x, y, Sum);
+            Console.ReadLine();
+        }
+        public static void MinusFunc(double x, double y)
+        {
+            double Min = x - y;
+            Console.WriteLine("{0} вычесть {1} = {2}", x, y, Min);
+            Console.ReadLine();
+        }
+        public static void UmnojitFunc(double x, double y)
+        {
+            double Umn = x * y;
+            Console.WriteLine("{0} умножить на {1} = {2}", x, y, Umn);
+            Console.ReadLine();
+        }
+        public static void RazdelitFunc(double x, double y)
+        {
+            double Razd = x / y;
+            Console.WriteLine("{0} разделить {1} = {2}", x, y, Razd);
+            Console.ReadLine();
+        }
+        public static void StepenFunc(double x, double y)
+        {
+            double Step = Math.Pow(x,y);
+            Console.WriteLine("{0} в степени {1} = {2}", x, y, Step);
+            Console.ReadLine();
+        }
+    }
+}
